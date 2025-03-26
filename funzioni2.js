c6eef1aabd08171d861b83a3ec44d93d00661eb6
danieledambr <danieledambr@gmail.com>
2025-03-26 18:01:59 +0100

    Add files via upload

--- funzioni2.js
+++ funzioni2.js
@@ -3,13 +3,13 @@ function FizzByzz(n) {
 
     for (let i = 1; i <= n; i++) {
        if (i % 15 == 0) {
-        console.log(`Fizz`);
+        console.log(`FizzBuzz`);
         
     } else if (i % 5 == 0){
-          console.log(`Buzz`);
+          console.log(`Fizz`);
           
     } else if (i % 3 == 0){
-        console.log(`FizzBuzz`);
+        console.log(`Buzz`);
     } else {
         console.log(i);
         
